61b9fd82f94d530a8c252b611aeb0cf61fea701b
enkhhuslen <enkhhuslenbatmunkh@gmail.com>
2023-05-08 08:49:19 +0800

    1575. Matrix Diagonal Sum

---
+++ easy/matrix_diagonal_sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let sum = 0;
+    for(var i=0; i<mat.length; i++){
+        for(var j=0; j<mat[i].length; j++){
+            if(i == j){
+                sum = sum + mat[i][j];
+            } else if(i + j == mat[i].length - 1){
+                sum = sum + mat[i][j];
+            }
+
+        }
+    }
+
+    return sum
+};
+
+// 0 0
+// 1 1
+// 2 2
+// 3 3
+
+// 0 3
+// 1 2
+// 2 1
+// 3 0
